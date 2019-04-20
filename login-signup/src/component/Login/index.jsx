f14f4c269ea0c0330655dc85ebded16ca70745e2
alaabadra <alaabadra4@gmail.com>
2019-04-20 16:57:52 +0300

    create filestrure and routes

---
+++ login-signup/src/component/Login/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+// import Logo from '../Logo'
+
+export default class Login extends Component {
+  render(){
+      return(
+         <div>login</div>
+      )
+  }
+}
